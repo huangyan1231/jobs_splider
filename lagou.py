5796df226d9bc78bd9610edc226200c26dae19fc
huangyan1231 <1432613064@qq.com>
2017-10-10 17:48:00 -0700

    详情页爬取

--- lagou.py
+++ lagou.py
@@ -5,6 +5,8 @@ import time
 import re
 import random
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from database.base_db import Session
 from models.model import Company, Position, Proxys
 session = Session()
@@ -38,21 +40,18 @@ class Scrapy(object):
             'User-Agent': ua,
             'Referer': 'https://www.lagou.com/jobs/list_%E4%BC%9A%E8%AE%A1?labelWords=sug&fromSearch=true&suginput=%E4%BC%9A%E8%AE%A1'
         }
-    # header = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-    #     'Referer': 'https://www.lagou.com/jobs/list_%E4%BC%9A%E8%AE%A1?labelWords=sug&fromSearch=true&suginput=%E4%BC%9A%E8%AE%A1'
-    # }
+
+    def sleep(self, s):
+        print('休眠:{}秒'.format(s))
+        time.sleep(s)
+
     def __init__(self, keyword):
         self.keyword = keyword
-        self.init_proxys()
-
-    def init_proxys(self):
-        
         proxy_list = session.query(Proxys).all()
         for proxy in proxy_list:
             self.proxies.append({
                 'http': 'http://{}:{}'.format(proxy.ip, proxy.port)
-            })
+            }) 
 
     def fetch_one(self, page=0):
         url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E6%88%90%E9%83%BD&needAddtionalResult=false&isSchoolJob=0'
@@ -62,13 +61,16 @@ class Scrapy(object):
             'pn': str(page),
             'kd': self.keyword
         }
- 
-        result = requests.post(url, data=params, headers=self.header, proxies=proxy, timeout=5).json()
+        try:
+            result = requests.post(url, data=params, headers=self.header, proxies=proxy, timeout=5).json()
+        except Exception:
+            proxy = random.choice(self.proxies)
+            result = requests.post(url, data=params, headers=self.header, proxies=proxy, timeout=10).json()
 
         if result['success'] == False:
             print(', 抓取失败')
             print(result)
-            # time.sleep(60)
+            self.sleep(60)
             return self.fetch_one(page)
         
         return result
@@ -81,7 +83,6 @@ class Scrapy(object):
             page_num = self.pages // 15
         print('共{}页'.format(page_num), end='')
         for page in range(1, page_num + 1):
-            # time.sleep(6)
             print(
                 '正在抓取第{}页'.format(
                     page
@@ -89,14 +90,23 @@ class Scrapy(object):
                 end=''
             )
             result = self.fetch_one(page)
-            self.parse(result['content']['positionResult']['result'])
+            self.parse(result['content']['positionResult']['result'], page)
+            self.sleep(3)
         print('{}职位抓取完成.'.format(self.keyword))
-    def parse(self, jobs):
-        for job in jobs:
+    def parse(self, jobs, page):
+        for i in range(0, len(jobs)):
+            job = jobs[i]
             # if position has been crawled
             cp = session.query(Company).filter_by(id=job['companyId']).first()
             ps = session.query(Position).filter_by(id=job['positionId']).first()
             if ps is None:
+                while True:
+                    try:
+                        job_detail = self.parse_job_detail(job['positionId'])
+                        break;
+                    except Exception:
+                        self.sleep(60)
+                        job_detail = self.parse_job_detail(job['positionId'])
                 position = Position(
                     id=job['positionId'],
                     company_id=job['companyId'],
@@ -114,13 +124,13 @@ class Scrapy(object):
                     district=job['district'],
                     first_type=job['firstType'],
                     second_type=job['secondType'],
-                    # job_advantage=job_detail['job_advantage'],
-                    # description=job_detail['description'],
-                    # location=job_detail['location'],
-                    # publisher_name=job_detail['publisher_name'],
-                    # tend_to_talk=job_detail['tend_to_talk'],
-                    # deal_resume=job_detail['deal_resume'],
-                    # active_time=job_detail['active_time']
+                    job_advantage=job_detail['job_advantage'],
+                    description=job_detail['description'],
+                    location=job_detail['location'],
+                    publisher_name=job_detail['publisher_name'],
+                    tend_to_talk=job_detail['tend_to_talk'],
+                    deal_resume=job_detail['deal_resume'],
+                    active_time=job_detail['active_time']
                 )
                 session.add(position)
                 session.commit()
@@ -136,13 +146,15 @@ class Scrapy(object):
                 )
                 session.add(company)
                 session.commit()
-
-        print(', 抓取成功')
+            print('正在抓取第{}页，第{}条记录'.format(page, i+1))
     
     def parse_job_detail(self, job_id):
-        # time.sleep(5)
         fetch_url = 'https://www.lagou.com/jobs/{}.html'.format(job_id)
-        html = requests.get(fetch_url, headers=self.header).text
+        detail_driver = webdriver.Chrome()
+        detail_driver.get(fetch_url)
+        self.sleep(15)
+        html = detail_driver.page_source
+        detail_driver.close()
         soup = BeautifulSoup(html,'lxml')
         job_advantage = None
         description = None
@@ -152,60 +164,43 @@ class Scrapy(object):
         deal_resume = None
         active_time = None
         # 职位诱惑
-        try:
-            job_advantage = soup.select('#job_detail')[0].select('.job-advantage')[0].select('p')[0].text
-        except IndexError:
-            print(html)
-            print('职位诱惑抓取失败')
-            pass
+        job_advantage = soup.select('#job_detail')[0].select('.job-advantage')[0].select('p')[0].text
         # 职位描述
-        try:
-            description_tmp = soup.select('#job_detail')[0].select('dd.job_bt div')[0].select('p')
-            description = [x.text for x in description_tmp if x.text != '']
-        except Exception:
-            print('职位描述抓取失败')
-            pass
+        
+        description_tmp = soup.select('#job_detail')[0].select('dd.job_bt div')[0].select('p')
+        description = [x.text for x in description_tmp if x.text != '']
+        
         # 工作区域
-        try:
-            location = re.sub('[/\s查看地图]', '', soup.select('.work_addr')[0].text)
-        except IndexError:
-            print('工作区域抓取失败')
-            pass
+        
+        location = re.sub('[/\s查看地图]', '', soup.select('.work_addr')[0].text)
+        
         
         # 发布者
-        try:
-            publisher_name = soup.select('.publisher_name .name')[0].text
-        except IndexError:
-            print('发布者抓取失败')
-            pass
+        
+        publisher_name = soup.select('.publisher_name .name')[0].text
+        
         
         # 聊天意愿
-        try:
-            tend_to_talk = dict()
-            tend_content = soup.select('.publisher_data div')[0]
-            tend_to_talk['step'] = tend_content.select('.data')[0].text
-            tend_to_talk['percent'] = tend_content.select('.tip')[0].select('i')[0].text
-            tend_to_talk['time'] = tend_content.select('.tip')[0].select('i')[1].text
-        except IndexError:
-            print('聊天意愿抓取失败')
-            pass
         
-        try:
-            deal_resume = dict()
-            resume_content = soup.select('.publisher_data div')[1]
-            deal_resume['step'] = resume_content.select('.data')[0].text
-            deal_resume['percent'] = resume_content.select('.tip')[0].select('i')[0].text
-            deal_resume['time'] = resume_content.select('.tip')[0].select('i')[1].text
-        except IndexError:
-            print('简历处理速度抓取失败')
-            pass
+        tend_to_talk = dict()
+        tend_content = soup.select('.publisher_data div')[0]
+        tend_to_talk['step'] = tend_content.select('.data')[0].text
+        tend_to_talk['percent'] = tend_content.select('.tip')[0].select('i')[0].text
+        tend_to_talk['time'] = tend_content.select('.tip')[0].select('i')[1].text
+        
+        
+        
+        deal_resume = dict()
+        resume_content = soup.select('.publisher_data div')[1]
+        deal_resume['step'] = resume_content.select('.data')[0].text
+        deal_resume['percent'] = resume_content.select('.tip')[0].select('i')[0].text
+        deal_resume['time'] = resume_content.select('.tip')[0].select('i')[1].text
+        
             
         # 活跃时间--上午，下午，中午
-        try:
-            active_time = soup.select('.publisher_data div')[1].select('.data')[0].text
-        except IndexError:
-            print('活跃时间抓取失败')
-            pass
+        
+        active_time = soup.select('.publisher_data div')[1].select('.data')[0].text
+        
         
         return {
             'job_advantage': job_advantage,
@@ -248,4 +243,4 @@ if __name__ == '__main__':
         print('关键字{}, '.format(pos), end='')
         scrapy = Scrapy(pos)
         scrapy.spider()
-        # time.sleep(70)
+        scrapy.sleep(70)
